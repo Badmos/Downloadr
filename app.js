6dd7cb206e1d77728637395bbfb0a576d25293f3
Badmos Yusuf <Badmosy@gmail.com>
2019-05-21 15:55:23 +0100

    added compression for faster load time

---
+++ app.js
@@ -0,0 +1,22 @@
+const compression = require('compression'),
+    express = require('express'),
+    favicon = require('serve-favicon'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    app = express();
+
+const routes = require('./routes/routes')
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(favicon(path.join(__dirname, "/public", "/images", "/favicon.ico"))); //Serve favicon
+app.set('view engine', 'ejs');
+app.use('/', routes);
+
+let PORT = process.env.PORT || 6060;
+
+app.listen(PORT, () => {
+    console.log(`running on ${PORT}`)
+})
